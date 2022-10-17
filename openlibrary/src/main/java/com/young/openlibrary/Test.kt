25b4a9e98af1467bfca6347913af9adf78f71f11
wangyang <wy6689806>
2022-10-17 15:53:48 +0800

    add tset

---
+++ openlibrary/src/main/java/com/young/openlibrary/Test.kt
@@ -0,0 +1,15 @@
+package com.young.openlibrary
+
+/**
+ * Time：2022/10/17 3:52 下午
+ * Author:wangyang
+ * Description:
+ */
+class Test {
+    val c = 10.add(20)
+}
+
+
+fun Int.add(a: Int): Int {
+    return this + a
+}
